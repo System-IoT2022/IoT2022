dd7a765294efd40c19aa2813fc7ef9e79e027812
giuseppe pintus <giuseppe.pintus2@studio.unibo.it>
2022-10-04 16:18:17 +0200

    first base for assignment

---
+++ assignment-01/assignment-01.ino
@@ -0,0 +1,25 @@
+#define WAKE_UP_PIN 2//external button for waking systems
+short int state;
+
+void setup() {
+  // put your setup code here, to run once:
+  attachInterrupt(digitalPinToInterrupt(WAKE_UP_PIN), wake, RISING); 
+}
+void wake(){
+  state = 1;
+} 
+void loop() {
+  // put your main code here, to run repeatedly:
+  switch(state) {
+    case 1://initial state
+    // code block
+      break;
+    case 2:
+    // code block
+      break;
+    default://sleepmode
+    // create new sequence
+    //
+  }
+
+}
