c3938c549f670404e50f65f5cb009eb3324e20a2
giuseppe pintus <giuseppe.pintus2@studio.unibo.it>
2022-10-05 09:21:29 +0200

    added setup for leds

--- assignment-01/assignment-01.ino
+++ assignment-01/assignment-01.ino
@@ -1,20 +1,29 @@
+#include <avr/sleep.h>
+
 #define WAKE_UP_PIN 2//external button for waking systems
 short int state;
-
+int led[4];
+int button[4];
 void setup() {
   // put your setup code here, to run once:
   attachInterrupt(digitalPinToInterrupt(WAKE_UP_PIN), wake, RISING); 
+  for(int i=0;i<4;i++){
+      led[i]=10+i;
+      button[i]=6+i;
+  }
 }
 void wake(){
   state = 1;
   sleep_disable();
+  
 } 
 void loop() {
   // put your main code here, to run repeatedly:
   switch(state) {
     case 1://initial state
       noInterrupts();
-    // code block
+    // new sequence list
+
       break;
     case 2:
     // code block
