5bcef4bd224b2f15afc2ffe476c513b75d2576cc
giuseppe pintus <giuseppe.pintus2@studio.unibo.it>
2023-02-22 10:17:46 +0100

    cleaned code

--- assignment-03/room-sensor-board/room-sensor-board.ino
+++ assignment-03/room-sensor-board/room-sensor-board.ino
@@ -1,26 +1,81 @@
-#include "Scheduler.h"
-#include "PirTask.h"
-//#include "Config.h"
 
-void setup(){
-  //WI-FI conn
-  Serial.begin(9600);
-  Scheduler::init(PERIOD);
-  //TaskController* t0 = new TaskController();
-  Task* pirSensor = new PirTask();
-  pirSensor->init(1000);
-  /*
-  only when pir detect change state
-  TAsk* ligthSensor = new Task()
-  ligthSensor->init(60000);
-  if(true){
-    ligthSensor->setActive(true);
+#include "Pirimpl.h"
+#include "LightSensorImpl.h"
+#include "Led.h"
+#include "Config.h"
+#define THL 120
+#define LIGHT_SENSOR_PIN 5
+#define PIR_SENSOR_PIN 5
+#define LED_PIN 5
+TaskHandle_t pirSensor;
+TaskHandle_t ligthSensor;
+
+bool state;
+PirImpl* pir;
+LightSensor* lightSensor;
+Led* led;
+
+
+
+
+/* pirSensor with priority 1 */
+static void pirSensorTask(void* pvParameters) {
+  bool newState = pir->isDetected();
+  if (state != newState) {
+    state = newState;
+    if (state) {
+      led->switchOn();
+      //mqtt message there are people
+      if (ligthSensor == NULL) {
+        xTaskCreate(ligthSensorTask, "ligthSensor", 100, NULL, 2, &ligthSensor);
+      }
+    } else {
+      led->switchOff();
+      vTaskDelete(ligthSensor);
+      //mqtt message there aren't people
+    }
   }
-  */
-  Scheduler::addTask(pirSensor);
-
 }
 
-void loop(){
-  Scheduler::schedule();
+
+/* ligthSensor with priority 2 */
+static void ligthSensorTask(void* pvParameters) {
+  if (lightSensor->getLightIntensity() <= THL) {
+    //mqtt message
+  }
+}
+
+void setup() {
+  //WI-FI conn
+  Serial.begin(115200);
+  setup_wifi();
+  randomSeed(micros());
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  state = false;
+  pir = new PirImpl(PIR_SENSOR_PIN);
+  led = new Led(LED_PIN);
+  lightSensor = new LightSensorImpl(LIGHT_SENSOR_PIN);
+  xTaskCreate(pirSensorTask, "pirSensor", 100, NULL, 1, &pirSensor);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  unsigned long now = millis();
+  if (now - lastMsgTime > 10000) {
+    lastMsgTime = now;
+    value++;
+
+    /* creating a msg in the buffer */
+    snprintf (msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
+
+    Serial.println(String("Publishing message: ") + msg);
+    
+    /* publishing the msg */
+    client.publish(topic, msg);  
+  }
 }
