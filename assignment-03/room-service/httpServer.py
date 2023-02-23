e7760f1fdae6e5313ef1867c19325a715a932c14
Song Zhaohui <zhaohui.song@studio.unibo.it>
2023-02-23 10:58:32 +0100

    add logger into both server

--- assignment-03/room-service/httpServer.py
+++ assignment-03/room-service/httpServer.py
@@ -3,6 +3,7 @@
 import socket               # Import socket module
 import datetime
 import json
+from serverLogger import *
 
 s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
@@ -13,18 +14,15 @@ s.bind((host, port))        # Bind to the port
 s.listen(5)                 # Now wait for client connection.
 
 # output = 'Thank you for connecting'
-date = '2021-05-21 11:22:03'
-datem = datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
-date1 = '2021-05-21 13:22:03'
-daten  = datetime.datetime.strptime(date1, "%Y-%m-%d %H:%M:%S")
-date = f'{daten.hour}:{daten.minute}:{daten.second}'
-date1  = f'{datem.hour}:{datem.minute}:{datem.second}'
-output = [{date : 'on'}, {date1 : 'off'}]
-data = json.dumps(output)
+logger = ServerLogger('light.txt')
+
+def getData():
+   lines = logger.read()
+   return json.dumps(lines)
 
 def run():
    c, addr = s.accept()     # Establish connection with client.
-   c.sendall(bytes(data, encoding="utf-8"))
+   c.sendall(bytes(getData(), encoding="utf-8"))
    c.close()                # Close the connection
 
 if __name__ == '__main__':
